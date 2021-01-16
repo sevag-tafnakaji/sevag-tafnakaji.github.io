9e5fbabc9aaf33ff239e3f737d45cb50d810a0c2
Sevag Tafnakaji <sevag.tafnakaji@gmail.com>
2021-01-16 14:06:09 +0100

    added project page

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,11 @@
 let overlay = document.getElementById("startScreen");
-let overlayText = document.getElementById("startText");
+
+let delay = 2;
+
+overlay.style.transition = `opacity ${delay}s`;
+overlay.style.transitionTimingFunction = "ease-in-out";
 
 overlay.onclick = function(){
     overlay.style.opacity = 0;
-    overlayText.style.opacity = 0;
+    setTimeout(function(){overlay.remove()}, delay*1000-300)
 };
