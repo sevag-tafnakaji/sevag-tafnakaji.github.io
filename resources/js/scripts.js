1f374ebca1a6988f98c7b87ac237c091c1a652b7
Sevag Tafnakaji <sevag.tafnakaji@gmail.com>
2021-01-16 13:11:21 +0100

    added overlay with transition

---
+++ resources/js/scripts.js
@@ -0,0 +1,7 @@
+let overlay = document.getElementById("startScreen");
+let overlayText = document.getElementById("startText");
+
+overlay.onclick = function(){
+    overlay.style.opacity = 0;
+    overlayText.style.opacity = 0;
+};
